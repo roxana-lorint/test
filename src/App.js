99c2440dcfb054ab8bc843e93b9e92caa54c31ab
Roxana Cestari <roxanacestari@Roxanas-MacBook-Pro.local>
2024-04-22 22:21:04 +0200

    change

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn
         </a>
+        <span>Hello</span>
       </header>
     </div>
   );
